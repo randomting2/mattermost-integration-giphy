0b09885d5138e200c4d30d41eeeba4370c4ff7bf
randomting <D-8logdau@maildrop.cc>
2016-08-08 16:31:22 +0200

    get rid of useless stuff

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ from mattermost_giphy.settings import *
 
 
 if __name__ == "__main__":
-    if not GIPHY_API_KEY:
-        print("GIPHY_API_KEY must be configured. Please see README.md for instructions")
-        sys.exit()
+#    if not GIPHY_API_KEY:
+#        print("GIPHY_API_KEY must be configured. Please see README.md for instructions")
+#        sys.exit()
 
-    if not MATTERMOST_GIPHY_TOKEN:
-        print("MATTERMOST_GIPHY_TOKEN must be configured. Please see README.md for instructions")
-        sys.exit()
+#    if not MATTERMOST_GIPHY_TOKEN:
+#        print("MATTERMOST_GIPHY_TOKEN must be configured. Please see README.md for instructions")
+#        sys.exit()
 
     port = os.environ.get('MATTERMOST_GIPHY_PORT', None) or os.environ.get('PORT', 5000)
     host = os.environ.get('MATTERMOST_GIPHY_HOST', None) or os.environ.get('HOST', '0.0.0.0')

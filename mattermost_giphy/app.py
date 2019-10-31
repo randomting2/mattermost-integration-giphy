ebd156ecedae2e62396bb1de081867299f05e304
randomting2 <43443761+randomting2@users.noreply.github.com>
2019-10-31 09:25:57 +0100

    Update app.py

--- mattermost_giphy/app.py
+++ mattermost_giphy/app.py
@@ -55,25 +55,23 @@ def new_post():
         if data.get('channel_name')==u'bingochan':
             if datetime.datetime.today().weekday() == 3:
                 if data.get('text').lower()==u'in':
-                    print('<div title="player-name-{}">{}</div> !'.format((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%Y-%m-%d')                                                                      
-        ,data.get('user_name').title()) ) 
-                    if (int((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))>=10) and (int((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))<=13): 
+                    if (int((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))>=10) and (int((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))<=13): 
                         print('<div title="player-name-{}">{}</div> !'.format((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%Y-%m-%d')                                                                          
         ,data.get('user_name').title()) )
                         print("la")
                         resp_data['text'] = '''`{}` joined the game! Be ready at  1:45p.m.\n'''.format(data.get('user_name').title())
-                    elif (int((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))<10):
-                        print((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))
+                    elif (int((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))<10):
+                        print((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))
                         resp_data['text'] = '''`{}` is a little too soon! See ya later!\n'''.format(data.get('user_name').title())
                     else:
-                        print((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))
+                        print((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))
                         resp_data['text'] = '''`{}` is a little too late! See ya next week!\n'''.format(data.get('user_name').title())  
                 elif data.get('text').lower()==u'bingo!':
-                    if (int((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))>=13) and (int((datetime.datetime.today()).strftime('%M'))>=00):                
+                    if (int((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))>=13) and (int((datetime.datetime.today()).strftime('%M'))>=00):                
                         print('<div title="winner-name-{}">{}_{}</div> !'.format((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%Y-%m-%d')                                                                          
             ,data.get('user_name').title(),(datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%Y-%m-%d-%H-%M-%s'))) 
                         resp_data['text'] = '''`{}` just claimed a bingo! \n'''.format(data.get('user_name').title())
-                    elif (int((datetime.datetime.today()+datetime.timedelta(hours=+2)).strftime('%H'))>13):
+                    elif (int((datetime.datetime.today()+datetime.timedelta(hours=+1)).strftime('%H'))>13):
                         resp_data['text'] = '''It's too late to bingo !\n'''.format(data.get('user_name').title())
                     else:
                         resp_data['text'] = '''It's too soon to bingo !\n'''.format(data.get('user_name').title())                        
@@ -82,7 +80,6 @@ def new_post():
             else:
                 resp_data['text'] = '''Sorry `{}`, there is no bingo today, see you on Thursday!\n'''.format(data.get('user_name').title())     
         else:
-            print("10")
             bo = False
     except Exception as err:
         msg = err.message
